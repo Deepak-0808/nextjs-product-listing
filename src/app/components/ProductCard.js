75fefdebaa37ec3a0b2a2ce13588f52c27f78456
Deepak Kumar <i.deepak0808@gmail.com>
2024-06-15 15:02:34 +0530

    first commit

--- src/app/components/ProductCard.js
+++ src/app/components/ProductCard.js
@@ -6,7 +6,7 @@ const ProductCard = ({ product }) => {
     <div className={styles.card}>
       <img src={product.image} alt={product.title} className={styles.image} />
       <h2 className={styles.title}>{product.title}</h2>
-      <p className={styles.price}>${product.price}</p>
+      <p className={styles.price}>₹{product.price}(50% off)</p>
     </div>
   );
 };
